a869cdeeef1977f3459b7c4ceb0abadb2c88e866
RishabhShukla04 <150980226+RishabhShukla04@users.noreply.github.com>
2024-08-30 23:48:37 +0100

    very simple etch a sketch, just getting used to having keybinds

---
+++ etch-a-sketch_project/main.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+
+tom = Turtle()
+
+
+
+screen = Screen()
+
+def move_forwards():
+    tom.forward(10)
+def move_left():
+    tom.setheading(tom.heading() + 10)
+def move_right():
+    tom.setheading(tom.heading() - 10)
+def move_backwards():
+    tom.backward(10)
+
+def clear():
+    tom.clear()
+    tom.penup()
+    tom.home()
+    tom.pendown()
+
+
+screen.listen()
+screen.onkeypress(key='w', fun=move_forwards)
+screen.onkey(key='s', fun=move_backwards)
+screen.onkey(key='a', fun=move_left)
+screen.onkey(key='d', fun=move_right)
+screen.onkey(key='c', fun = clear)
+
+screen.exitonclick()
